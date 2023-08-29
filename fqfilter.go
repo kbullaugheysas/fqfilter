24f71cb385628432b466ff43a70d9e30066cad1f
Kevin Bullaughey <bullaugh@sas.upenn.edu>
2023-08-29 15:35:33 -0400

    -tab -short and -limit options

--- fqfilter.go
+++ fqfilter.go
@@ -18,6 +18,9 @@ type Args struct {
 	Invert        bool
 	ReadsFilename string
 	OutPrefix     string
+	Limit         int
+	Tab           bool
+	ShortName     bool
 }
 
 var args = Args{}
@@ -25,8 +28,11 @@ var args = Args{}
 func init() {
 	log.SetFlags(0)
 	flag.BoolVar(&args.Invert, "invert", false, "return reads NOT in the file")
+	flag.BoolVar(&args.Tab, "tab", false, "print sequence as tabular output (readName, read1, read2)")
+	flag.BoolVar(&args.ShortName, "short-name", false, "use just the first space-separated word of the read name")
 	flag.StringVar(&args.ReadsFilename, "reads", "", "filename of reads to match")
 	flag.StringVar(&args.OutPrefix, "out", "", "output filename prefix (default = stdout)")
+	flag.IntVar(&args.Limit, "limit", 0, "output only the first LIMIT matches")
 
 	flag.Usage = func() {
 		log.Println("usage: fqfilter [options] unaligned_1.fq.gz unaligned_2.fq.gz")
@@ -141,6 +147,10 @@ func main() {
 		log.Fatal("Must provide -reads <file> argument")
 	}
 
+	if len(fq) == 0 {
+		log.Fatal("Must specify at least one fastq file")
+	}
+
 	// Open the inputs
 	inputs := make([]AmbiReader, len(fq))
 	for i, fn := range fq {
@@ -150,23 +160,41 @@ func main() {
 		defer inputs[i].Close()
 	}
 
-	// Prepare the output writers
-	outputs := make([]AmbiWriter, len(fq))
-	for i := 0; i < len(fq); i++ {
-		if args.OutPrefix == "" {
-			outputs[i].Stdout()
-		} else {
-			fn := fmt.Sprintf("%s_%d.fq.gz", args.OutPrefix, i+1)
-			if err := outputs[i].Open(fn); err != nil {
-				log.Fatalf("Failed to open %s for writing: %v\n", fn, err)
+	var outputs []AmbiWriter
+
+	if args.Tab {
+		if args.OutPrefix != "" {
+			log.Fatal("Tabular output only supports writing to stdout")
+		}
+	} else {
+		// Prepare the output writers
+
+		outputs = make([]AmbiWriter, len(fq))
+		for i := 0; i < len(fq); i++ {
+			if args.OutPrefix == "" {
+				outputs[i].Stdout()
+			} else {
+				var fn string
+				if len(fq) == 1 {
+					fn = fmt.Sprintf("%s.fq.gz", args.OutPrefix)
+				} else {
+					fn = fmt.Sprintf("%s_%d.fq.gz", args.OutPrefix, i+1)
+				}
+				if err := outputs[i].Open(fn); err != nil {
+					log.Fatalf("Failed to open %s for writing: %v\n", fn, err)
+				}
+				defer outputs[i].Close()
 			}
-			defer outputs[i].Close()
 		}
 	}
 
 	// Read in the list of reads
 	reads := AmbiReader{}
-	if err := reads.Open(args.ReadsFilename); err != nil {
+	readsFn := args.ReadsFilename
+	if readsFn == "stdin" {
+		readsFn = ""
+	}
+	if err := reads.Open(readsFn); err != nil {
 		log.Fatalf("Failed to open %s: %v\n", args.ReadsFilename, err)
 	}
 	defer reads.Close()
@@ -175,6 +203,9 @@ func main() {
 	scanner := bufio.NewScanner(reads)
 	for scanner.Scan() {
 		name := scanner.Text()
+		if args.ShortName {
+			name = strings.Fields(name)[0]
+		}
 		filter[name] = true
 	}
 
@@ -182,6 +213,9 @@ func main() {
 	inputScanners := make([]*bufio.Scanner, len(fq))
 	for i := 0; i < len(fq); i++ {
 		inputScanners[i] = bufio.NewScanner(inputs[i])
+		/* Make sure we have a large buffer for long sequences */
+		buf := make([]byte, 0, 1024*1024)
+		inputScanners[i].Buffer(buf, 10*1024*1024)
 	}
 	var enable bool
 	if args.Invert {
@@ -192,6 +226,8 @@ func main() {
 	line_num := 0
 	included := 0
 	excluded := 0
+	var name string
+	sequences := make([]string, len(fq))
 	err := func() error {
 		for {
 			for i := 0; i < len(fq); i++ {
@@ -200,26 +236,43 @@ func main() {
 					if line_num%4 == 0 {
 						if strings.HasPrefix(line, "@") {
 							if i == 0 {
-								name := line[1:len(line)]
+								name = line[1:len(line)]
+								if args.ShortName {
+									name = strings.Fields(name)[0]
+								}
+								_, enable = filter[name]
 								if args.Invert {
-									_, hit := filter[name]
-									enable = !hit
-								} else {
-									_, enable = filter[name]
+									enable = !enable
 								}
 							}
 						} else {
-							return fmt.Errorf("Line %d should be a fastq header line, got: %s\n", line_num, line)
+							return fmt.Errorf("Line %d should be a header line, got: %s\n", line_num, line)
+						}
+					}
+					if line_num%4 == 1 {
+						sequences[i] = line
+						if i == 0 {
+							if enable {
+								included++
+							} else {
+								excluded++
+							}
 						}
 					}
 					if enable {
-						if _, err := io.WriteString(outputs[i], line); err != nil {
-							return fmt.Errorf("Failed to write line %d to output %d: %v\n", line_num, i, err)
+						if args.Tab {
+							if i+1 == len(fq) && line_num%4 == 1 {
+								outputLine := name
+								for j := 0; j < len(fq); j++ {
+									outputLine = outputLine + "\t" + sequences[j]
+								}
+								fmt.Println(outputLine)
+							}
+						} else {
+							if _, err := io.WriteString(outputs[i], line+"\n"); err != nil {
+								return fmt.Errorf("Failed to write line %d to output %d: %v\n", line_num, i, err)
+							}
 						}
-						io.WriteString(outputs[i], "\n")
-						included++
-					} else {
-						excluded++
 					}
 				} else {
 					if i == 0 {
@@ -230,6 +283,10 @@ func main() {
 				}
 			}
 			line_num++
+			if args.Limit > 0 && included >= args.Limit {
+				log.Println("reached limit")
+				return nil
+			}
 		}
 	}()
 	if err != nil {
